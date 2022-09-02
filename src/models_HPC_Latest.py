be835a2b8c14ae717183278267ec43247bb027fe
Roberto Perera Aguiar <65241735+rperera12@users.noreply.github.com>
2022-09-02 16:24:00 -0500

    Update models_HPC_Latest.py

--- src/models_HPC_Latest.py
+++ src/models_HPC_Latest.py
@@ -17,6 +17,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 from collections import OrderedDict
+import torch
+import os
+#from torch.utils.data import Dataset
+import torch_geometric
+from torch_geometric.data import Dataset
+from torch_geometric.data import Data
+import networkx as nx
+import matplotlib.pyplot as plt
+from config_HPC_Latest import args_generator
+import numpy as np
+import glob
+from scipy.spatial import Delaunay
+from collections import OrderedDict
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+#from progressbar import ProgressBar
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops, degree
+#from ogb.graphproppred import Evaluator
+#from ogb.graphproppred import PygGraphPropPredDataset as OGBG
+#from ogb.graphproppred.mol_encoder import AtomEncoder
+from torch.nn import BatchNorm1d, Linear, ReLU, Sequential
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import argparse
+import os.path as osp
+import torch.nn.utils.rnn as rnn_utils
+import torch_geometric.transforms as T
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import EGConv, global_mean_pool
+from typing import Optional, Tuple
+from torch import Tensor
+from torch.nn import Parameter
+from torch_sparse import SparseTensor, matmul
+from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.typing import Adj
+from tqdm import tqdm
+#from torch_geometric_temporal.signal import temporal_signal_split
+import seaborn as sns
+from torch_geometric_temporal.nn.recurrent import A3TGCN2 
 
 from config_HPC_Latest import args_generator
 
@@ -302,3 +342,78 @@ class Net6(torch.nn.Module):
         x = self.lin1(x).relu()
         out = self.lin2(x)
         return out
+    
+    
+class TemporalGNN(torch.nn.Module):
+    def __init__(self, node_features, periods, batch_size):
+        super(TemporalGNN, self).__init__()
+        # Attention Temporal Graph Convolutional Cell
+        self.tgnn = A3TGCN2(in_channels=node_features,  out_channels=32, periods=periods,batch_size=batch_size) # node_features=2, periods=12
+        # Equals single-shot prediction
+        self.linear = torch.nn.Linear(32, periods)
+
+    def forward(self, x, edge_index):
+        """
+        x = Node features for T time steps
+        edge_index = Graph edge indices
+        """
+        h = self.tgnn(x, edge_index) # x [b, 207, 2, 12]  returns h [b, 207, 12]
+        h = F.relu(h) 
+        h = self.linear(h)
+        return h
+
+
+if phase == 'train':
+    if args.GNN_Model == 'cPhi_GNN':
+        TemporalGNN(node_features=9, periods=1, batch_size=args.batch_size)
+    else:
+        TemporalGNN(node_features=7, periods=1, batch_size=args.batch_size)
+elif phase == 'eval':
+    if args.GNN_Model == 'cPhi_GNN':
+        TemporalGNN(node_features=9, periods=1, batch_size=1)
+    else:
+        TemporalGNN(node_features=7, periods=1, batch_size=4)
+
+
+
+# Create model and optimizers
+if torch.cuda.is_available() == True:
+    if phase == 'train':
+        if args.GNN_Model == 'cPhi_GNN':
+            model = TemporalGNN(node_features=9, periods=1, batch_size=args.batch_size).to(device)
+        else:
+            model = TemporalGNN(node_features=7, periods=1, batch_size=args.batch_size).to(device)
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+        loss_fn = torch.nn.MSELoss()
+
+        print('Net\'s state_dict:')
+        total_param = 0
+        for param_tensor in model.state_dict():
+            print(param_tensor, '\t', model.state_dict()[param_tensor].size())
+            total_param += np.prod(model.state_dict()[param_tensor].size())
+        print('Net\'s total params:', total_param)
+        #--------------------------------------------------
+        print('Optimizer\'s state_dict:')  # If you notice here the Attention is a trainable parameter
+        for var_name in optimizer.state_dict():
+            print(var_name, '\t', optimizer.state_dict()[var_name])
+
+
+    elif phase == 'eval':
+        if args.GNN_Model == 'cPhi_GNN':
+            model = TemporalGNN(node_features=9, periods=1, batch_size=1).to(device)
+            pretrained_dict = torch.load('save_dir/trained_cPhiGNN_Angles.pth')
+        elif args.GNN_Model == 'XDisp_GNN':
+            model = TemporalGNN(node_features=7, periods=1, batch_size=1).to(device)
+            pretrained_dict = torch.load('save_dir/trained_XDispGNN_NEW.pth') 
+        elif args.GNN_Model == 'YDisp_GNN':
+            model = TemporalGNN(node_features=7, periods=1, batch_size=1).to(device)
+            pretrained_dict = torch.load('save_dir/trained_YDispGNN_TEST.pth') 
+        model.load_state_dict(pretrained_dict, strict=False)
+    edge_index = edge_index.to(device)
+
+else:
+    if phase == 'train':
+        model = TemporalGNN(node_features=9, periods=1, batch_size=4)
+    elif phase == 'eval':
+        model = TemporalGNN(node_features=9, periods=1, batch_size=1)
